0f7d82e03c86a47909cf1bc781b30e55f2914c9f
valbonah <103250143+valbonah@users.noreply.github.com>
2023-12-04 22:57:59 +0100

    Update Student.cs

--- Student.cs
+++ Student.cs
@@ -1,3 +1,4 @@
+
 using LibraryProject;
 using System;
 using System.Collections.Generic;
@@ -5,11 +6,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
-namespace LibraryProject    
+namespace LibraryProject
 {
+    // The Student class represents a student who is a library member.
     public class Student : LibraryMember
     {
+        // Constructor for the Student class, taking the student's name as a parameter.
         public Student(string name) : base(name) { }
     }
 }
